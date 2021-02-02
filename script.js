50e2334a4aa7b5672a1d396d79b2fd41e65d33d1
LewisCode-Art <dzackalewis@gmail.com>
2021-02-02 13:17:17 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+Variables and data types
+*/
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Doe';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+
+/***************
+*Variables mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName);
